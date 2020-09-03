74942a00e858a6bd5ccbde83cb233d33015060a7
Nishanta Khanal <nkhanal@sig.sig-gis.com>
2020-09-03 15:59:23 -0700

    fix alert system

--- gmw/settings.py
+++ gmw/settings.py
@@ -146,8 +146,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CRON_CLASSES = [
     'subscribe.cron.GoldAlerts',
-    'subscribe.cron.CleanStaleProjects'
-    'subscribe.cron.CleanCorruptProjects'
+    # 'subscribe.cron.CleanStaleProjects'
+    # 'subscribe.cron.CleanCorruptProjects'
 ]
 
 #EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
--- subscribe/cron.py
+++ subscribe/cron.py
@@ -1,4 +1,5 @@
 from django_cron import CronJobBase, Schedule
+from django.db.models import Min, Max
 from subscribe.models import SubscribeModel, ProjectsModel
 from subscribe.utils import saveProject, projectExists
 from accounts.models import Profile
@@ -13,20 +14,26 @@ from subscribe import utils as subutils
 
 class GoldAlerts(CronJobBase):
     RUN_EVERY_MINS = 0.001 # every 0.001 min (doesn't run on it's timer but blocks if executed in less that that interval)
-    
+
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'gmw.cronalerts'    # a unique code
 
     def do(self):
-        return
         try:
             authGEE()
             latest_image, latest_date = getLatestImage()
-            users = SubscribeModel.objects.all().values('user').distinct()
-            for user in iter(users):
-                proj_created = subutils.createProject(user['user'], latest_date)
+            today = datetime.datetime.now().strftime("%Y-%m-%d")
+            users = SubscribeModel.objects.all().values('user').annotate(alert = Min('last_alert_for'))
+            alertables = filter(lambda x: x['alert'] < latest_date, users)
+            for alertable in iter(alertables):
+                user = alertable['user']
+                email = Profile.objects.filter(user=user).values().first()['email']
+                regions = '__'.join(subutils.getSubscribedRegions(user))
+                proj_created = subutils.createProject(user, latest_date, 'Alert-for-'+today, regions)
+
                 if (proj_created['action'] == "Created"):
-                    sendmail(email, projurl)
+                    SubscribeModel.objects.filter(user=user).update(last_alert_for=latest_date)
+                    sendmail(email, proj_created['proj'][3])
                     print('mail sent to ', email)
                 else:
                     print(proj_created)
