5381f8d88acf12868e34cff5769e11b1fa32cbc4
Matt Spencer <sirmspencer@gmail.com>
2021-04-08 11:16:52 -0700

    finalize serach panel changes

--- gmw/statics/js/SearchPanel.js
+++ gmw/statics/js/SearchPanel.js
@@ -7,23 +7,36 @@ class SearchPanel extends React.Component {
         };
 
         this.state = {
-            geoCodedSearch: [],
-            activeL1: false
-            // dataSetSearch: []
+            geoCodedSearch: null,
+            selectedL1: -1,
+            selectedL2: -1,
+            searchText: "",
+            latLngText: ""
         };
     }
 
-    searchGeocode = searchString => {
-        const url = this.URLS.GEOCODE + "key=" + mapquestkey + "&location=" + searchString;
+    componentDidUpdate() {
+        // do like other panels and refresh on open
+    }
+
+    // aipe
+    searchGeocode = () => {
+        const url = this.URLS.GEOCODE + "key=" + mapquestkey + "&location=" + this.state.searchText;
         fetch(url)
             .then(resp => resp.json())
             .then(result => {
+                // TODO filter by exists in list.
                 this.setState({
-                    geoCodedSearch: result.results[0].locations.filter(l => l.adminArea1 === "CO"
-                        && l.adminArea3
-                        && l.adminArea4
-                        && !l.adminArea5
-                        && !l.adminArea6)
+                    geoCodedSearch: result.results[0].locations.filter(l => {
+                        try {
+                            return l.adminArea1 === "CO"
+                                && this.props.featureNames[l.adminArea3.toUpperCase()][l.adminArea4.toUpperCase()]
+                                && !l.adminArea5
+                                && !l.adminArea6;
+                        } catch (err) {
+                            return false;
+                        }
+                    })
                 });
             })
             .catch(error => console.log(error));
@@ -46,9 +59,8 @@ class SearchPanel extends React.Component {
     // }
 
     inputChanged(key, value) {
-        if (key === "Enter") {
-            this.searchGeocode(value);
-        }
+        this.setState({searchText: value});
+        if (key === "Enter") this.searchGeocode();
         // if (value) {
         //     this.searchDataset(value);
         // } else {
@@ -56,43 +68,43 @@ class SearchPanel extends React.Component {
         // }
     }
 
-    latLngChanged = (key, value) => {
-        if (key === "Enter") {
-            const pair = value.split(",");
-            const nump = pair
-                .map(a => parseFloat(a))
-                .slice(0, 2);
-            this.props.fitMap("point", [nump[1], nump[0]]);
-        }
+    processLatLng = () => {
+        const pair = this.state.latLngText.split(",");
+        const nump = pair
+            .map(a => parseFloat(a))
+            .slice(0, 2);
+        this.props.fitMap("point", [nump[1], nump[0]]);
     };
 
-    stateSelected(value) {
-        this.setState({activeL1: value});
-    }
-
     render() {
-        const geoSearchResults = (
-            <div>{this.state.geoCodedSearch.slice(0, 3).map(item => (
-                <div
-                    key={item.adminArea3}
-                    className="search-results"
-                    onClick={() => {
-                        this.props.fitMap("point", [item.latLng.lng, item.latLng.lat]);
-                        this.props.selectRegion(
-                            "mun",
-                            item.adminArea3.toUpperCase()
+        const {geoCodedSearch} = this.state;
+        const geoSearchResults = geoCodedSearch && geoCodedSearch.length === 0
+            ? <label>No Results</label>
+            : geoCodedSearch && (
+                <div>{geoCodedSearch.slice(0, 3).map(item => (
+                    <div
+                        key={item.adminArea3}
+                        className="search-results"
+                        onClick={() => {
+                            const state = item.adminArea3.toUpperCase();
+                            const mun = item.adminArea4.toUpperCase();
+                            this.setState({selectedL1: state, selectedL2: mun});
+                            this.props.fitMap("bbox", this.props.featureNames[state][mun]);
+                            this.props.selectRegion(
+                                "mun",
+                                item.adminArea3.toUpperCase()
                                 + "_"
                                 + item.adminArea4.toUpperCase()
-                        );
-                    }}
-                    style={{display: "flex", flexDirection: "column"}}
-                >
-                    <span><b>{item.adminArea1}</b>&nbsp;<i>{item.adminArea4}, {item.adminArea3}</i></span>
-                    <span>{item.latLng.lat},{item.latLng.lng}</span>
+                            );
+                        }}
+                        style={{display: "flex", flexDirection: "column"}}
+                    >
+                        <span><b>{item.adminArea1}</b>&nbsp;<i>{item.adminArea4}, {item.adminArea3}</i></span>
+                        <span>{item.latLng.lat},{item.latLng.lng}</span>
+                    </div>
+                ))}
                 </div>
-            ))}
-            </div>
-        );
+            );
 
         const l1Names = Object.keys(this.props.featureNames).sort() || [];
         const selectL1 = l1Names.length > 0
@@ -101,19 +113,19 @@ class SearchPanel extends React.Component {
                     <small>State</small>
                     <select
                         className="w_100"
-                        defaultValue={0}
-                        onChange={e => this.stateSelected(e.target.value)}
+                        onChange={e => this.setState({selectedL1: e.target.value})}
+                        value={this.state.selectedL1}
                     >
-                        <option key={-1} disabled value={0}>Select a State</option>
-                        {l1Names.map(item => (
-                            <option key={item} value={item}>{item}</option>
+                        <option key={-1} disabled value={-1}>Select a State</option>
+                        {l1Names.map(name => (
+                            <option key={name} value={name}>{name}</option>
                         ))}
                     </select>
                 </div>
             )
             : "Loading ...";
 
-        const activeMuns = this.props.featureNames[this.state.activeL1] || {};
+        const activeMuns = this.props.featureNames[this.state.selectedL1] || {};
         const l2names = Object.keys(activeMuns).sort();
         const selectL2 = l2names.length > 0
             ? (
@@ -121,20 +133,21 @@ class SearchPanel extends React.Component {
                     <small>Municipality</small>
                     <select
                         className="w_100"
-                        defaultValue={0}
                         onChange={e => {
                             const name = e.target.value;
                             const coords = activeMuns[name];
+                            this.setState({selectedL2: name});
                             if (Array.isArray(coords)) this.props.fitMap("bbox", coords);
                             this.props.selectRegion(
                                 "mun",
-                                this.state.activeL1
+                                this.state.selectedL1
                                     + "_"
                                     + e.target.value
                             );
                         }}
+                        value={this.state.selectedL2}
                     >
-                        <option key={-1} disabled value={0}>Select a Municipality</option>
+                        <option key={-1} disabled value={-1}>Select a Municipality</option>
                         {l2names.map(item => (
                             <option key={item} value={item}>{item}</option>
                         ))}
@@ -146,10 +159,30 @@ class SearchPanel extends React.Component {
             <div className={"popup-container search-panel " + (this.props.isHidden ? "see-through" : "")}>
                 <h3>SEARCH LOCATION</h3>
                 <label>Internet Search</label>
-                <input className="w_100" onKeyUp={e => this.inputChanged(e.key, e.target.value)}/>
+                <div className="d-flex">
+                    <input
+                        className="w_100"
+                        onChange={e => this.setState({searchText: e.target.value})}
+                        onKeyUp={e => { if (e.key === "Enter") this.searchGeocode(); }}
+                        value={this.state.searchText}
+                    />
+                    <button className="map-upd-btn" onClick={this.searchGeocode} type="button">
+                        Go
+                    </button>
+                </div>
                 {geoSearchResults}
                 <label>Lat Long Search</label>
-                <input className="w_100" onKeyUp={e => this.latLngChanged(e.key, e.target.value)}/>
+                <div className="d-flex">
+                    <input
+                        className="w_100"
+                        onChange={e => this.setState({latLngText: e.target.value})}
+                        onKeyUp={e => { if (e.key === "Enter") this.processLatLng(); }}
+                        value={this.state.latLngText}
+                    />
+                    <button className="map-upd-btn" onClick={this.processLatLng} type="button">
+                        Go
+                    </button>
+                </div>
                 <label>Select Municipality</label>
                 {selectL1}
                 {selectL2}
