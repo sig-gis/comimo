9ed1cc2844c0c1cd5700dce14b7b0f0b0a09cec5
Matt Spencer <sirmspencer@gmail.com>
2022-02-15 16:14:54 -0800

    A few more initial migration changes

--- gmw/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from accounts.models import Profile
-
-from django.http import JsonResponse, HttpResponse
-from accounts.utils import getUserInfo
-
-
-def homeView(request):
-    return render(request, "home.html", getUserInfo(request.user))
