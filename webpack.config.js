224d877d823f80a5ea8d0260f270555c75d91646
Matt Spencer <sirmspencer@gmail.com>
2022-03-02 15:21:06 -0800

    Update packages and add styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const outdir = "target/public/js";
 
 module.exports = env => ({
   mode: env.dev ? "development" : "production",
-  devtool: env.dev ? "eval-cheap-module-source-map" : "",
+  devtool: env.dev ? "eval-cheap-module-source-map" : "eval",
   watch: env.dev,
   entry: {
     collect:         path.resolve(__dirname, "src/js/collect/collect.js"),
@@ -20,7 +20,8 @@ module.exports = env => ({
   },
   output: {
     path: path.resolve(__dirname, outdir),
-    filename: "[name].[hash].bundle.js",
+    filename: pathData => (pathData.chunk.name ? "[name].[chunkhash].bundle.js" : "[chunkhash].bundle.js"),
+    clean: true,
     chunkFilename: "[chunkhash].chunk.js",
     library: "[name]",
     libraryTarget: "var"
@@ -66,35 +67,24 @@ module.exports = env => ({
   plugins: [
     {
       apply: compiler => {
-        compiler.hooks.beforeCompile.tap("BeforeCompilePlugin", () => {
+        compiler.hooks.initialize.tap("InitializePlugin", () => {
           // Ensure that outdir exists.
           if (!fs.existsSync("./" + outdir)) {
             fs.mkdirSync("./" + outdir, {recursive: true});
           }
-          // Remove old files to prevent conflicts.
-          // Dev will have the old files removed at the beginning
+        });
+        compiler.hooks.beforeCompile.tap("BeforeCompilePlugin", () => {
+          // Remove old entry-points at the beginning
           // so a browser refresh does not show stale data.
           if (env.dev) {
             fs.unlink("./target/entry-points.json", () => null);
-            fs.readdirSync("./" + outdir)
-              .forEach(f => fs.unlinkSync(path.join("./" + outdir, f)));
-          }
-        });
-        compiler.hooks.emit.tap("EmitPlugin", () => {
-          // Remove old files to prevent conflicts.
-          // Production will have the old files removed immediately before
-          // the new ones are created for less interruption.
-          if (!env.dev) {
-            fs.unlink("./target/entry-points.json", () => null);
-            fs.readdirSync("./" + outdir)
-              .forEach(f => fs.unlinkSync(path.join("./" + outdir, f)));
           }
         });
         compiler.hooks.done.tap("DonePlugin", stats => {
           // Map entrypoint name -> chunk files
           const entryPoints = Array.from(stats.compilation.entrypoints.entries());
           const newMap = entryPoints.reduce((acc, [name, ep]) =>
-            ({...acc, [name]: ep.chunks.map(c => "/js/" + c.files[0])}),
+            ({...acc, [name]: ep.chunks.map(c => { const [file] = c.files; return "/js/" + file; })}),
                                             {});
           fs.writeFile("./target/entry-points.json", JSON.stringify(newMap), "utf8", e => {
             if (e) console.error(e);
