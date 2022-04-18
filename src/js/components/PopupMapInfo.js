ae265cf19c0240c2f8de7902ac645a95916de943
Sif Biri <sbiri@sig-gis.com>
2022-04-18 09:03:10 -0700

    Add PopupMapInfo

---
+++ src/js/components/PopupMapInfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const PopupMapInfo = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: start;
+`;
+
+export default PopupMapInfo;
