1cc305311a0ac932a5212003191e8bd65fe482b6
Sif Biri <sbiri@sig-gis.com>
2022-06-16 08:48:13 -0700

    Added support for language defaults

--- src/clj/comimo/views.clj
+++ src/clj/comimo/views.clj
@@ -1,35 +1,41 @@
 (ns comimo.views
-  (:require [clojure.data.json :as json]
-            [clojure.string    :as str]
-            [clojure.java.io   :as io]
-            [cognitect.transit :as transit]
-            [hiccup.page :refer [html5 include-js include-css]]
-            [triangulum.config  :refer [get-config]])
+  (:require [clojure.data.json          :as json]
+            [clojure.string             :as str]
+            [clojure.java.io            :as io]
+            [cognitect.transit          :as transit]
+            [hiccup.page                :refer [html5 include-js include-css]]
+            [triangulum.config          :refer [get-config]]
+            [triangulum.database        :refer [call-sql]])
   (:import java.io.ByteArrayOutputStream))
 
 (defn kebab->camel [kebab]
   (let [pieces (str/split kebab #"-")]
     (apply str (first pieces) (map str/capitalize (rest pieces)))))
 
-(defn head [extra-js]
-  [:head
-   [:title "Colombian Mining Monitoring"]
-   [:meta {:charset "utf-8"}]
-   [:meta {:name "viewport" :content "width=device-width, user-scalable=no"}]
-   [:meta {:name "description" :content "Colombian Mining Monitoring (CoMiMo) is an online mining monitoring application that uses machine learning and satellite imagery to alert government authorities, NGOs and concerned citizens about possible mining activities anywhere in Colombia, and enables them to assess the location, lawfulness and potential impacts to the environment of those mines."}]
-   [:meta {:name "keywords" :content "colombian mining monitoring, comimo, satellite imagery, illegal mining, machine learning, image analysis, detection, crowdsourcing"}]
-   [:link {:rel "shortcut icon" :href "favicon.ico"}]
-   (when-let [ga-id (get-config :ga-id)]
-     (list [:script {:async true :src (str "https://www.googletagmanager.com/gtag/js?id=" ga-id)}]
-           [:script (str "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" ga-id "', {'page_location': location.host + location.pathname});")]))
-   (include-css "css/bootstrap-grid.min.4.3.1.css"
-                "css/gmw_global.css"
-                "css/gmw_main.css" ; only home
-                )
-   (apply include-js
-          "/js/google-charts.js"
-          "/js/jquery-3.4.1.min.js"
-          extra-js)])
+(defn head [extra-js lang]
+  (let [description {:en "Colombian Mining Monitoring (CoMiMo) is an online mining monitoring application that uses machine learning and satellite imagery to alert government authorities, NGOs and concerned citizens about possible mining activities anywhere in Colombia, and enables them to assess the location, lawfulness and potential impacts to the environment of those mines."
+                     :es "Monitoreo Minero Colombiano (CoMiMo) es una aplicación de monitoreo minero en línea que utiliza aprendizaje automático e imágenes satelitales para alertar a las autoridades gubernamentales, ONGs y ciudadanos preocupados sobre posibles actividades mineras en cualquier lugar de Colombia, y les permite evaluar la ubicación, la legalidad y los posibles impactos al entorno de esas minas.
+"}
+        keywords    {:en "colombian mining monitoring, comimo, satellite imagery, illegal mining, machine learning, image analysis, detection, crowdsourcing"
+                     :es "monitoreo minero colombiano, comimo, imágenes satelitales, minería ilegal, aprendizaje automático, análisis de imágenes, detección, crowdsourcing"}]
+    [:head
+     [:title "Colombian Mining Monitoring"]
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, user-scalable=no"}]
+     [:meta {:name "description" :content (description lang)}]
+     [:meta {:name "keywords" :content (keywords lang)}]
+     [:link {:rel "shortcut icon" :href "favicon.ico"}]
+     (when-let [ga-id (get-config :ga-id)]
+       (list [:script {:async true :src (str "https://www.googletagmanager.com/gtag/js?id=" ga-id)}]
+             [:script (str "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" ga-id "', {'page_location': location.host + location.pathname});")]))
+     (include-css "css/bootstrap-grid.min.4.3.1.css"
+                  "css/gmw_global.css"
+                  "css/gmw_main.css"    ; only home
+                  )
+     (apply include-js
+            "/js/google-charts.js"
+            "/js/jquery-3.4.1.min.js"
+            extra-js)]))
 
 (defn uri->page [uri]
   (->> (str/split uri #"/")
@@ -39,10 +45,10 @@
 
 (defn js-init [page params]
   (let [js-params (-> params
-                      (assoc
-                       :mapboxToken (get-config :mapbox-token)
-                       :mapquestKey (get-config :mapquest-key))
-                      (json/write-str))]
+                    (assoc
+                      :mapboxToken (get-config :mapbox-token)
+                      :mapquestKey (get-config :mapquest-key))
+                    (json/write-str))]
     [:script {:type "text/javascript"}
      (str "window.onload = function () {" page ".pageInit(" js-params "); };")]))
 
@@ -93,22 +99,24 @@
 (defn render-page [uri]
   (fn [request]
     (let [page          (uri->page uri)
-          webpack-files (find-webpack-files page)]
+          webpack-files (find-webpack-files page)
+          user-id       (-> request :params :userId)
+          lang          (-> (call-sql "get_user_information" user-id) first (:default-lang :en))]
       {:status  200
        :headers {"Content-Type" "text/html"}
        :body    (html5
-                 (head webpack-files)
-                 [:body
-                  (if (seq webpack-files)
-                    [:section
-                     ;; TODO These will be moved to the front end for better UX.
-                     (when-let [flash-message (get-in request [:params :flash_message])]
-                       [:p {:class "alert"} flash-message])
-                     (when (.exists (io/as-file "announcement.txt"))
-                       (announcement-banner))
-                     [:div#main-container]]
-                    [:label "No webpack files found. Check if webpack is running, or wait for it to finish compiling."])
-                  (js-init page (:params request))])})))
+                  (head webpack-files lang)
+                  [:body
+                   (if (seq webpack-files)
+                     [:section
+                      ;; TODO These will be moved to the front end for better UX.
+                      (when-let [flash-message (get-in request [:params :flash_message])]
+                        [:p {:class "alert"} flash-message])
+                      (when (.exists (io/as-file "announcement.txt"))
+                        (announcement-banner))
+                      [:div#main-container]]
+                     [:label "No webpack files found. Check if webpack is running, or wait for it to finish compiling."])
+                   (js-init page (:params request))])})))
 
 (defn not-found-page [request]
   (-> request
@@ -129,17 +137,17 @@
   ([body]
    (data-response body {}))
   ([body {:keys [status type session]
-          :or {status 200 type :json}
-          :as params}]
+          :or   {status 200 type :json}
+          :as   params}]
    (merge (when (contains? params :session) {:session session})
-          {:status  status
-           :headers {"Content-Type" (condp = type
-                                      :edn     "application/edn"
-                                      :transit "application/transit+json"
-                                      :json    "application/json"
-                                      type)}
-           :body    (condp = type
-                      :edn     (pr-str         body)
-                      :transit (body->transit  body)
-                      :json    (json/write-str body)
-                      body)})))
+     {:status  status
+      :headers {"Content-Type" (condp = type
+                                 :edn     "application/edn"
+                                 :transit "application/transit+json"
+                                 :json    "application/json"
+                                 type)}
+      :body    (condp = type
+                 :edn     (pr-str         body)
+                 :transit (body->transit  body)
+                 :json    (json/write-str body)
+                 body)})))
