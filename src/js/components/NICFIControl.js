17034cbfec66cb4c041527f63aa17a2a244576a2
Oliver-BE <obaldwinedwards@gmail.com>
2022-07-18 16:15:03 -0700

    SER-51 Separate Planet NICFI from other layers

--- src/js/components/NICFIControl.js
+++ src/js/components/NICFIControl.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import Button from "./Button";
 import Select from "./Select";
 
-export default function NICFIControl({extraParams, setParams, nicfiLayers}) {
+export default function NICFIControl({ extraParams, setParams, nicfiLayers }) {
   const [selectedTime, setTime] = useState("");
   const [selectedBand, setBand] = useState("");
   useEffect(() => {
@@ -14,16 +14,19 @@ export default function NICFIControl({extraParams, setParams, nicfiLayers}) {
   }, [extraParams]);
 
   return (
-    <div className="flex flex-col pl-3">
+    <div className="flex flex-col">
       <Select
         id="time"
         label="Select Time"
-        onChange={e => setTime(e.target.value)}
-        options={nicfiLayers.map(time => ({value: time, label: time.slice(34, time.length - 7)}))}
+        onChange={(e) => setTime(e.target.value)}
+        options={nicfiLayers.map((time) => ({
+          value: time,
+          label: time.slice(34, time.length - 7),
+        }))}
         value={selectedTime}
       />
       <div className="flex flex-col">
-        <label className="mb-0 mr-3">Select Band</label>
+        <label className="ml-0">Select Band</label>
         <div className="flex">
           <div>
             <input
@@ -32,9 +35,7 @@ export default function NICFIControl({extraParams, setParams, nicfiLayers}) {
               onChange={() => setBand("rgb")}
               type="radio"
             />
-            <label htmlFor="visible">
-            Visible
-            </label>
+            <label htmlFor="visible">Visible</label>
           </div>
           <div className="pl-2">
             <input
@@ -43,23 +44,20 @@ export default function NICFIControl({extraParams, setParams, nicfiLayers}) {
               onChange={() => setBand("cir")}
               type="radio"
             />
-            <label htmlFor="infrared">
-            Infrared
-            </label>
+            <label htmlFor="infrared">Infrared</label>
           </div>
         </div>
       </div>
       <Button
-        className="mt-4"
-        onClick={() => setParams(
-          "NICFI",
-          {
+        className="mt-2"
+        onClick={() =>
+          setParams("NICFI", {
             dataLayer: selectedTime,
-            band: selectedBand
-          }
-        )}
+            band: selectedBand,
+          })
+        }
       >
-          Update Map
+        Update NICFI Layer
       </Button>
     </div>
   );
--- src/js/home/LayersPanel.js
+++ src/js/home/LayersPanel.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {MainContext} from "../components/PageLayout";
+import { MainContext } from "../components/PageLayout";
 
 import NICFIControl from "../components/NICFIControl";
 import ToolPanel from "../components/ToolPanel";
 
-import {startVisible, availableLayers} from "../constants";
+import { startVisible, availableLayers } from "../constants";
 
 export default class LayersPanel extends React.Component {
   constructor(props) {
@@ -12,36 +12,41 @@ export default class LayersPanel extends React.Component {
 
     this.state = {
       visible: null,
-      opacity: null
+      opacity: null,
     };
   }
 
   componentDidMount() {
     this.setState({
-      visible: availableLayers.reduce((acc, cur) => ({...acc, [cur]: startVisible.includes(cur) || false}), {}),
-      opacity: availableLayers.reduce((acc, cur) => ({...acc, [cur]: 100}), {})
+      visible: availableLayers.reduce(
+        (acc, cur) => ({ ...acc, [cur]: startVisible.includes(cur) || false }),
+        {}
+      ),
+      opacity: availableLayers.reduce((acc, cur) => ({ ...acc, [cur]: 100 }), {}),
     });
   }
 
   setVisible = (name, layerVisible) => {
-    const {visible} = this.state;
-    const {theMap} = this.props;
+    const { visible } = this.state;
+    const { theMap } = this.props;
     theMap.setLayoutProperty(name, "visibility", layerVisible ? "visible" : "none");
-    this.setState({visible: {...visible, [name]: layerVisible}});
+    this.setState({ visible: { ...visible, [name]: layerVisible } });
   };
 
   setOpacity = (name, newOpacity) => {
-    const {opacity} = this.state;
-    const {theMap} = this.props;
+    const { opacity } = this.state;
+    const { theMap } = this.props;
     theMap.setPaintProperty(name, "raster-opacity", newOpacity / 100);
     this.setState({
-      opacity: {...opacity, [name]: newOpacity}
+      opacity: { ...opacity, [name]: newOpacity },
     });
   };
 
-  renderControl = name => {
-    const {opacity, visible} = this.state;
-    const {localeText: {layers}} = this.context;
+  renderControl = (name) => {
+    const { opacity, visible } = this.state;
+    const {
+      localeText: { layers },
+    } = this.context;
     const layerVisible = visible[name];
     return (
       <div key={name} className="d-flex justify-content-between align-items-center mb-2">
@@ -52,14 +57,16 @@ export default class LayersPanel extends React.Component {
             onChange={() => this.setVisible(name, !layerVisible)}
             type="checkbox"
           />
-          <label htmlFor={"label-" + name} style={{margin: "0 0 3px 0"}}>{layers[name]}</label>
+          <label htmlFor={"label-" + name} style={{ margin: "0 0 3px 0" }}>
+            {layers[name]}
+          </label>
         </div>
         <input
           className="p-0 m-0"
           max="100"
           min="0"
-          onChange={e => this.setOpacity(name, parseInt(e.target.value))}
-          style={{width: "40%"}}
+          onChange={(e) => this.setOpacity(name, parseInt(e.target.value))}
+          style={{ width: "40%" }}
           type="range"
           value={opacity[name]}
         />
@@ -67,30 +74,50 @@ export default class LayersPanel extends React.Component {
     );
   };
 
-  renderControlWrapper = name => (name === "NICFI"
-    ? (
-      <div key={name} className="d-flex flex-column">
-        {this.renderControl(name)}
+  renderHeading = (layers) => (
+    <>
+      <div className="d-flex justify-content-between mb-0">
+        <label style={{ fontWeight: "bold", margin: "0 .25rem 0 0" }}>{layers.nameLabel}</label>
+        <label style={{ fontWeight: "bold", margin: "0 .25rem", width: "40%" }}>
+          {layers.opacityLabel}
+        </label>
+      </div>
+      <hr style={{ marginBottom: "0.5rem" }}></hr>
+    </>
+  );
+
+  renderNICFISection = () => (
+    <>
+      <label style={{ fontWeight: "bold", margin: "0 .25rem 0 0" }}>
+        Updated Satellite Imagery
+      </label>
+      <hr style={{ marginBottom: "0.5rem" }}></hr>
+      <div className="d-flex flex-column">
+        {this.renderControl("NICFI")}
         <NICFIControl
           extraParams={this.props.extraParams}
           nicfiLayers={this.props.nicfiLayers}
           setParams={this.props.setParams}
         />
       </div>
-    ) : this.renderControl(name));
+    </>
+  );
 
   render() {
-    const {opacity, visible} = this.state;
-    const {localeText: {layers}} = this.context;
+    const { opacity, visible } = this.state;
+    const {
+      localeText: { layers },
+    } = this.context;
     return (
       <ToolPanel title={layers.title}>
-        <div className="d-flex justify-content-between mb-2">
-          <label style={{margin: "0 .25rem"}}>{layers.nameLabel}</label>
-          <div style={{width: "40%"}}>
-            <label style={{margin: "0 .25rem"}}>{layers.opacityLabel}</label>
-          </div>
-        </div>
-        {opacity && visible && availableLayers.map(l => this.renderControlWrapper(l))}
+        {this.renderHeading(layers)}
+        {opacity &&
+          visible &&
+          availableLayers.map((layerName) =>
+            layerName === "NICFI" ? "" : this.renderControl(layerName)
+          )}
+        <br></br>
+        {opacity && visible && this.renderNICFISection("NICFI")}
       </ToolPanel>
     );
   }
