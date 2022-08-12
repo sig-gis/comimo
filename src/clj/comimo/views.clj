d9840f89a3d7e7ecb97641ec249fe8df938b015a
Sif Biri <sif@fastmail.com>
2022-08-11 17:45:15 -0700

    Update backend index files

--- src/clj/comimo/views.clj
+++ src/clj/comimo/views.clj
@@ -15,7 +15,25 @@
   (let [pieces (str/split kebab #"-")]
     (apply str (first pieces) (map str/capitalize (rest pieces)))))
 
-(defn head [extra-js lang]
+(defn find-bundle-js-files [page]
+  (let [src-file        (format "src/js/%s.jsx" page)
+        manifest        (-> (slurp "dist/public/manifest.json")
+                            (json/read-str)
+                            (get src-file))
+        entrypoint-file (str "/" (get manifest "file"))
+        asset-files     (mapv #(str/replace % #"^_" "/assets/")
+                              (get manifest "imports"))]
+    (conj asset-files entrypoint-file)))
+
+(defn find-bundle-css-files []
+  (let [manifest        (-> (slurp "dist/public/manifest.json")
+                            (json/read-str))
+        asset-css-files (->> manifest
+                             (filter (fn [[k _v]] (str/includes? k ".css")))
+                             (map (fn [[_k v]] (str "/" (get v "file")))))]
+    asset-css-files))
+
+(defn head [bundle-js-files bundle-css-files lang]
   (let [title       {:en "Colombian Mining Monitoring"
                      :es "Monitoreo Minero Colombiano"}
         description {:en "Colombian Mining Monitoring (CoMiMo) is an online mining monitoring application that uses machine learning and satellite imagery to alert government authorities, NGOs and concerned citizens about possible mining activities anywhere in Colombia, and enables them to assess the location, lawfulness and potential impacts to the environment of those mines."
@@ -25,31 +43,58 @@
     [:head
      [:title (title lang)]
      [:meta {:charset "utf-8"}]
-     [:meta {:name "viewport" :content "width=device-width, user-scalable=no"}]
-     [:meta {:name "description" :content (description lang)}]
-     [:meta {:name "keywords" :content (keywords lang)}]
+     [:meta {:name    "viewport"
+             :content "width=device-width, user-scalable=no"}]
+     [:meta {:name    "description"
+             :content (description lang)}]
+     [:meta {:name    "keywords"
+             :content (keywords lang)}]
      ; Favicon entries
-     [:link {:rel "apple-touch-icon" :sizes "180x180" :href "/favicon/apple-touch-icon.png"}]
-     [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/favicon/favicon-32x32.png"}]
-     [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/favicon/favicon-16x16.png"}]
-     [:link {:rel "manifest" :href "/favicon/site.webmanifest"}]
-     [:link {:rel "mask-icon" :color "#5bbad5" :href "/favicon/safari-pinned-tab.svg"}]
-     [:link {:rel "shortcut icon" :href "/favicon/favicon.ico"}]
-     [:meta {:name "msapplication-TileColor" :content "#ffc40d"}]
-     [:meta {:name "msapplication-config" :content "/favicon/browserconfig.xml"}]
-     [:meta {:name "theme-color" :content "#ffffff"}]
+     [:link {:rel   "apple-touch-icon"
+             :sizes "180x180"
+             :href  "/favicon/apple-touch-icon.png"}]
+     [:link {:rel   "icon"
+             :type  "image/png"
+             :sizes "32x32"
+             :href  "/favicon/favicon-32x32.png"}]
+     [:link {:rel   "icon"
+             :type  "image/png"
+             :sizes "16x16"
+             :href  "/favicon/favicon-16x16.png"}]
+     [:link {:rel  "manifest"
+             :href "/favicon/site.webmanifest"}]
+     [:link {:rel   "mask-icon"
+             :color "#5bbad5"
+             :href  "/favicon/safari-pinned-tab.svg"}]
+     [:link {:rel  "shortcut icon"
+             :href "/favicon/favicon.ico"}]
+     [:meta {:name    "msapplication-TileColor"
+             :content "#ffc40d"}]
+     [:meta {:name    "msapplication-config"
+             :content "/favicon/browserconfig.xml"}]
+     [:meta {:name    "theme-color"
+             :content "#ffffff"}]
      ; end Favicon
+     ;; TODO add  dev  loading handling (hot reload) config
+
      (when-let [ga-id (get-config :ga-id)]
-       (list [:script {:async true :src (str "https://www.googletagmanager.com/gtag/js?id=" ga-id)}]
+       (list [:script {:async true
+                       :src   (str "https://www.googletagmanager.com/gtag/js?id=" ga-id)}]
              [:script (str "window.dataLayer = window.dataLayer || []; function gtag() {dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" ga-id "', {'page_location': location.host + location.pathname});")]))
-     (include-css "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-                  "css/gmw_global.css"
-                  "css/gmw_main.css"       ; only home
-                  "css/tailwindOutput.css")
-     (apply include-js
-            "https://www.gstatic.com/charts/loader.js"
-            "/js/jquery-3.4.1.min.js"
-            extra-js)]))
+
+     (apply include-css
+            "https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+            ;; TODO: all this to be gone
+            "/css/gmw_global.css"
+            "/css/gmw_main.css"                         ; only home
+            "/css/tailwindOutput.css"
+            bundle-css-files)
+     (include-js
+      "https://www.gstatic.com/charts/loader.js"
+      "/js/jquery-3.4.1.min.js")
+     (map (fn [f] [:script {:type "module"
+                            :src  f}])
+          bundle-js-files)]))
 
 (defn uri->page [uri]
   (->> (str/split uri #"/")
@@ -57,23 +102,21 @@
        (first)
        (kebab->camel)))
 
-(defn js-init [page params]
-  (let [js-params (-> params
-                      (assoc
-                       :mapboxToken (get-config :mapbox-token)
-                       :mapquestKey (get-config :mapquest-key)
-                       :version (current-version))
-                      (json/write-str))]
-    [:script {:type "text/javascript"}
-     (str "window.onload = function () {" page ".pageInit(" js-params "); };")]))
-
-(defn find-webpack-files [page]
-  (as-> (slurp "target/entry-points.json") wp
-    (json/read-str wp)
-    (get wp page)))
+(defn js-init [entry-file params]
+  (let [js-params  (-> params
+                       (assoc
+                        :mapboxToken (get-config :mapbox-token)
+                        :mapquestKey (get-config :mapquest-key)
+                        :version (current-version))
+                       (json/write-str))
+        script-str (str
+                    "import {pageInit} from \"." entry-file "\";"
+                    "window.onload = function () { pageInit(" js-params "); };")]
+    [:script {:type "module"}
+     script-str]))
 
 (defn- announcement-banner []
-  (let [announcement (slurp "announcement.txt")] ; TODO This will be moved to the front end for better UX.
+  (let [announcement (slurp "announcement.txt")]            ; TODO This will be moved to the front end for better UX.
     [:div#banner {:style {:background-color "#f96841"
                           :box-shadow       "3px 1px 4px 0 rgb(0, 0, 0, 0.25)"
                           :color            "#ffffff"
@@ -107,32 +150,34 @@
                          :top              "5px"
                          :width            "25px"}
                :onClick "document.getElementById('banner').style.display='none'"}
-      [:svg {:viewBox "0 0 48 48" :fill "#ffffff"}
+      [:svg {:viewBox "0 0 48 48"
+             :fill    "#ffffff"}
        [:path {:d "M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83
                      11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"}]]]]))
 
 (defn render-page [uri]
   (fn [request]
-    (let [page          (uri->page uri)
-          webpack-files (find-webpack-files page)
-          user-id       (-> request :params :userId)
-          user          (first (call-sql "get_user_information" user-id))
-          lang          (:default-lang user :en)]
+    (let [page             (uri->page uri)
+          bundle-js-files  (find-bundle-js-files page)
+          bundle-css-files (find-bundle-css-files)
+          user-id          (-> request :params :userId)
+          user             (first (call-sql "get_user_information" user-id))
+          lang             (:default-lang user :en)]
       {:status  200
        :headers {"Content-Type" "text/html"}
        :body    (html5
-                 (head webpack-files lang)
+                 (head bundle-js-files bundle-css-files lang)
                  [:body
-                  (if (seq webpack-files)
+                  (if (seq bundle-js-files)
                     [:section
-                     ;; TODO These will be moved to the front end for better UX.
+                      ;; TODO These will be moved to the front end for better UX.
                      (when-let [flash-message (get-in request [:params :flash_message])]
                        [:p {:class "alert"} flash-message])
                      (when (.exists (io/as-file "announcement.txt"))
                        (announcement-banner))
                      [:div#main-container]]
-                    [:label "No webpack files found. Check if webpack is running, or wait for it to finish compiling."])
-                  (js-init page (:params request))])})))
+                    [:label "No JS bundle files found. Check if your bundler is running, or wait for it to finish compiling."])
+                  (js-init (last bundle-js-files) (:params request))])})))
 
 (defn not-found-page [request]
   (-> request
@@ -153,17 +198,18 @@
   ([body]
    (data-response body {}))
   ([body {:keys [status type session]
-          :or   {status 200 type :json}
+          :or   {status 200
+                 type   :json}
           :as   params}]
    (merge (when (contains? params :session) {:session session})
           {:status  status
            :headers {"Content-Type" (condp = type
-                                      :edn     "application/edn"
+                                      :edn "application/edn"
                                       :transit "application/transit+json"
-                                      :json    "application/json"
+                                      :json "application/json"
                                       type)}
            :body    (condp = type
-                      :edn     (pr-str         body)
-                      :transit (body->transit  body)
-                      :json    (json/write-str body)
+                      :edn (pr-str body)
+                      :transit (body->transit body)
+                      :json (json/write-str body)
                       body)})))
