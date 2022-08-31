d687d6e4aca3beaaf8c8ce0da5bad569001ea41d
Oliver-BE <obaldwinedwards@gmail.com>
2022-08-31 13:41:35 -0700

    SER-262 Update 404 page

--- src/js/pageNotFound.jsx
+++ src/js/pageNotFound.jsx
@@ -9,6 +9,7 @@ import { THEME } from "./constants";
 
 const PageContainer = styled.div`
   align-items: center;
+  background-color: #313035;
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -17,8 +18,16 @@ const PageContainer = styled.div`
   width: 100%;
 `;
 
+const LogoImg = styled.img`
+  cursor: pointer;
+  height: auto;
+  width: 100%;
+`;
+
 const HeaderContainer = styled.h1`
-  font-size: 60px;
+  color: var(--yellow-brand);
+  font: var(--unnamed-font-style-normal) normal bold 32px/32px var(--unnamed-font-family-roboto);
+  text-transform: uppercase;
 `;
 
 function PageNotFound() {
@@ -30,6 +39,11 @@ function PageNotFound() {
     <ThemeProvider theme={THEME}>
       <PageContainer>
         <HeaderContainer>{home?.pageNotFound}</HeaderContainer>
+        <LogoImg
+          alt="404-logo"
+          src="/img/comimo404.jpg"
+          onClick={() => window.location.assign("/")}
+        />
       </PageContainer>
     </ThemeProvider>
   );
