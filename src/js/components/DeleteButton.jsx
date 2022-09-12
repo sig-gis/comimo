ff18b02c0f1ea5dc33b0fcaf6c03f2a4144f849f
Sif Biri <sif@fastmail.com>
2022-09-12 11:25:30 -0700

    Remove unecessary return statement

--- src/js/components/DeleteButton.jsx
+++ src/js/components/DeleteButton.jsx
@@ -3,16 +3,14 @@ import React from "react";
 import IconButton from "./IconButton";
 
 function DeleteButton({ onClick }) {
-  return (
-    <IconButton
-      extraStyle={{ borderColor: "red" }}
-      bgColorHover="#ff40409e"
-      size="15px"
-      icon="redX"
-      tooltip="remove"
-      onClick={onClick}
-    />
-  );
+  <IconButton
+    extraStyle={{ borderColor: "red" }}
+    bgColorHover="#ff40409e"
+    size="15px"
+    icon="redX"
+    tooltip="remove"
+    onClick={onClick}
+  />;
 }
 
 export default DeleteButton;
