f59078d7d70fb20fc6069d709eac0f151d2b47f7
Oliver-BE <obaldwinedwards@gmail.com>
2022-09-12 11:35:59 -0700

    Small fix

--- src/js/components/DeleteButton.jsx
+++ src/js/components/DeleteButton.jsx
@@ -3,14 +3,16 @@ import React from "react";
 import IconButton from "./IconButton";
 
 function DeleteButton({ onClick }) {
-  <IconButton
-    extraStyle={{ borderColor: "red" }}
-    bgColorHover="#ff40409e"
-    size="15px"
-    icon="redX"
-    tooltip="remove"
-    onClick={onClick}
-  />;
+  return (
+    <IconButton
+      extraStyle={{ borderColor: "red" }}
+      bgColorHover="#ff40409e"
+      size="15px"
+      icon="redX"
+      tooltip="remove"
+      onClick={onClick}
+    />
+  );
 }
 
 export default DeleteButton;
