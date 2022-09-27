8abd00838838d7ff0ab860b0a9054414691087a1
Sif Biri <sif@fastmail.com>
2022-09-27 10:19:19 -0700

    Update sliders style

--- src/js/home/LayersPanel.jsx
+++ src/js/home/LayersPanel.jsx
@@ -24,10 +24,9 @@ const LayerCheckbox = styled.input`
 `;
 
 const sliderThumb = (layerColor) => css`
-  background: ${layerColor || "var(--teal-1)"};
-  border: 2px solid;
-  border-color: var(--white);
-  border-radius: 50%;
+  background: var(--white);
+  border: 4px solid;
+  border-color: ${layerColor || "var(--teal - 1)"};
   box-shadow: 0px 3px 2px #00000040;
   cursor: ew-resize;
   height: 18px;
