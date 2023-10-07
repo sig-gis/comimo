90fb5c962e1cc2f610db99be513319007aa3b75f
Dan Whitaker <dan@uncharted.systems>
2023-10-07 05:34:19 -0600

    integrated pulse jobs

--- src/clj/comimo/jobs.clj
+++ src/clj/comimo/jobs.clj
@@ -1,24 +1,15 @@
 (ns comimo.jobs
   (:require [comimo.db.subscriptions :refer [send-email-alerts]]
+            [pulse.jobs              :refer [start-scheduled-service!]]
             [triangulum.database     :refer [call-sql]]
             [triangulum.logging      :refer [log-str]]))
 
-(def ^:private expires-in "1 day in msecs" (* 1000 60 60 24))
-
-(defn- scheduled-jobs []
+(defn- refresh-alerts [] ; TODO: find more applicable name, batch?
   (log-str "Closing old projects")
   (call-sql "close_old_projects" 30)
   (log-str "Checking for alerts")
   (send-email-alerts))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn start-scheduling-service! []
-  (log-str "Starting scheduling service.")
-  (future
-    (while true
-      (Thread/sleep expires-in)
-      (try (scheduled-jobs)
-           (catch Exception _)))))
+(def ^:private one-day "1 day in msecs" (* 1000 60 60 24))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(defn stop-scheduling-service! [service] (future-cancel service))
+(def refresh-alerts-service #(start-scheduled-service! #'refresh-alerts one-day true))
